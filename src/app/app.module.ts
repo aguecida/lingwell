f2eaa9ede0b53e2083abee5df944599a7347e5c1
Daniele Agueci <d.agueci1@gmail.com>
2018-10-21 14:04:02 -0400

    Fix build errors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { MaterialModule } from './material.module';
@@ -25,8 +25,12 @@ import { ShadowScrollDirective } from './directives/shadow-scroll/shadow-scroll.
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
+    ReactiveFormsModule,
     MaterialModule,
     RouteModule
+  ],
+  entryComponents: [
+    FeedbackComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
