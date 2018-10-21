ece3f546e13c35500dd09b0388882d5c94cb1f4f
Daniele Agueci <dagueci@upstreamworks.com>
2018-10-20 23:31:36 -0400

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lwl-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'lingwell';
+}
