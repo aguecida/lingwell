50deb9f1d8bcf49f7cef51084d10098e391bdf49
Daniele Agueci <d.agueci1@gmail.com>
2018-11-02 19:44:23 -0400

    Update Angular to v7

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SplashPageComponent } from './components/splash-page/splash-page.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { PrimerComponent } from './components/primer/primer.component';
 
 const routes: Routes = [
   {
@@ -10,7 +11,7 @@ const routes: Routes = [
   },
   {
     path: 'getting-started',
-    loadChildren: './primer/primer.module#PrimerModule'
+    component: PrimerComponent
   },
   {
     path: '**',
